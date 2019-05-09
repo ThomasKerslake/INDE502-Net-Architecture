553d3ed494e1382baa3c3a485c9f45614c75ba4e
ThomasKerslake <33869524+ThomasKerslake@users.noreply.github.com>
2019-05-09 12:47:55 +0100

    Small change to db EJS

--- INDE502LoginPassport-master/passportStart/config/dbconfig.js
+++ INDE502LoginPassport-master/passportStart/config/dbconfig.js
@@ -1,3 +1,10 @@
 module.exports = {
+  MongoURI: 'mongodb+srv://Tom:admin@inde502-rlrnl.mongodb.net/test?retryWrites=true'
+};
+
+
+/*
+module.exports = {
   MongoURI: 'mongodb://localhost:27017/users'
 };
+*/
